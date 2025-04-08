a99770aa74d49fed49371b557c707929789df315
IsabelyFaria <bellyfaria04@gmail.com>
2025-04-08 11:27:51 -0300

    continuação codigo exercicio 1

--- exercicio01.php
+++ exercicio01.php
@@ -6,7 +6,20 @@
     <title>Exercicio 1</title>
 </head>
 <body>
-    
+
+<fieldset>
+        <legend>Exercicio 1</legend>
+            
+            <form method="post" action="calcular.php">
+                Informe o valor em dinheiro: <br/>
+                <input type="text" name="valor" size="25" /> <br/>
+                <br/>
+
+                <input type="submit" name="info" value="Enviar" />
+            </form>
+    </fieldset>
+
+
     <?php
 $valor = (int)readline("Digite um valor em dinheiro: ");
 

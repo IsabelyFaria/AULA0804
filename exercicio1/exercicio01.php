e4a317c274611c67486c92304b6b3ee9fb7558e3
IsabelyFaria <bellyfaria04@gmail.com>
2025-04-08 11:29:19 -0300

    alterando

--- exercicio1/exercicio01.php
+++ exercicio1/exercicio01.php
@@ -18,5 +18,30 @@
                 <input type="submit" name="info" value="Enviar" />
             </form>
     </fieldset>
+
+
+    <?php
+$valor = (int)readline("Digite um valor em dinheiro: ");
+
+$notas = [100, 50, 20, 10, 5, 2, 1];
+$resultado = [];
+
+    foreach ($notas as $nota) 
+    {
+        $quantidade = intdiv($valor, $nota);
+        $valor %= $nota;
+        $resultado[$nota] = $quantidade;
+    }
+
+        echo "Notas necessárias:\n";
+        foreach ($resultado as $nota => $quantidade) 
+        {
+            if ($quantidade > 0) 
+             {
+                echo "R$$nota,00: $quantidade\n";
+             }
+        }
+
+?>
 </body>
 </html>

f3919f3297e89b8d32f2310be4bea6adca2f0899
GeovanaCunha <geovanahcunha1109@gmail.com>
2025-04-11 08:15:57 -0300

    feito exercicio 2

---
+++ exercicio2/calcular.php
@@ -0,0 +1,14 @@
+<?php
+
+$numeroUsuario = (int)readline("Digite um número: ");
+$contador = 0;
+
+while ($numeroUsuario > 0) {
+    $numeroAleatorio = rand(1, 10);
+    $numeroUsuario -= $numeroAleatorio;
+    $contador++;
+}
+
+echo "O processo foi executado $contador vezes até o valor ser igual ou menor que zero.\n";
+
+?>
--- exercicio2/exercicio2.php
+++ exercicio2/exercicio2.php
@@ -1,1 +1,24 @@
-# comit 
+# 2 - Crie um programa que receba um número inteiro positivo do usuário e, enquanto o número for maior que zero, subtraia um número aleatório entre 1 e 10 desse número.
+# O programa deve contar quantas vezes isso foi feito e exibir o resultado.
+
+<!DOCTYPE html>
+<html lang="pt br"> 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 2</title>
+</head>
+<body>
+<fieldset>
+        <legend>Exercicio 2 - Contador de Subtrações</legend>
+            
+            <form method="post" action="calcular.php">
+                Informe um número inteiro positivo: <br/>
+                <input type="text" name="numeroUsuario" size="25" /> <br/>
+                <br/>
+
+                <input type="submit" name="info" value="Enviar" />
+            </form>
+    </fieldset>
+</body>
+</html>
